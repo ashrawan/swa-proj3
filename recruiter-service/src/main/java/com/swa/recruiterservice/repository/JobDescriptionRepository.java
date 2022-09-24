74bfd6de91465324c348b637bcf31b79688dfdcd
jhonr <sir.edwards@gmail.com>
2022-09-23 19:28:42 -0500

    removed joboperation that should be in job service

--- recruiter-service/src/main/java/com/swa/recruiterservice/repository/JobDescriptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.swa.recruiterservice.repository;
-
-import com.swa.recruiterservice.domain.JobDescription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobDescriptionRepository extends JpaRepository<JobDescription, Long> {
-}
