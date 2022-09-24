99c89e73bf2151d6021d2f6ebf4cef204ecd65df
sanjayakoju <sanjayakoju42@gmail.com>
2022-09-24 10:21:59 -0500

    working on candidate-service (create mongodb compose file)

--- proj3-common-module/src/main/java/com/swa/proj3commonmodule/dto/CandidateDTO.java
+++ proj3-common-module/src/main/java/com/swa/proj3commonmodule/dto/CandidateDTO.java
@@ -1,10 +1,14 @@
 package com.swa.proj3commonmodule.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CandidateDTO {
 
     private String candidateID;
