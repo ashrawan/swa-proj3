bd647d3861f486038c9fc66e108c275e90ab023e
ashrawan <shrawan.adhikari.31@gmail.com>
2022-09-28 21:26:51 -0500

    fixing some code for default security configs

--- search-service-elastic/src/main/java/com/example/searchserviceelastic/SearchServiceElasticApplication.java
+++ search-service-elastic/src/main/java/com/example/searchserviceelastic/SearchServiceElasticApplication.java
@@ -1,33 +1,24 @@
 package com.example.searchserviceelastic;
 
-import com.example.searchserviceelastic.entity.JobEntity;
-import com.example.searchserviceelastic.service.JobService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-import java.util.UUID;
-
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class SearchServiceElasticApplication {
-
-    @Autowired
-    private JobService jobService;
 
     public static void main(String[] args) {
         SpringApplication.run(SearchServiceElasticApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner() {
-        return args -> {
-            JobEntity jobEntity = new JobEntity();
-            jobEntity.setJobId(UUID.randomUUID().toString());
-            jobEntity.setDescription("Test description");
-            jobEntity.setTitle("Software Developer");
-            jobService.save(jobEntity);
-        };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(JobService jobService) {
+//        return args -> {
+//            JobEntity jobEntity = new JobEntity();
+//            jobEntity.setJobId(UUID.randomUUID().toString());
+//            jobEntity.setDescription("Test description");
+//            jobEntity.setTitle("Software Developer");
+//            jobService.save(jobEntity);
+//        };
+//    }
 }
